098c59b1aa264347458d754a583839721d9e1408
Ian Joiner <iajoiner809@gmail.com>
2022-07-03 15:49:14 -0400

    refactor: move proto file

--- orc-rs/build.rs
+++ orc-rs/build.rs
@@ -1,6 +1,6 @@
 extern crate prost_build;
 
 fn main() {
-    prost_build::compile_protos(&["../proto/orc_proto.proto"],
+    prost_build::compile_protos(&["src/proto/orc_proto.proto"],
                                 &["src/"]).unwrap();
 }
